813a8090a613aaeb947bc75c7565a0c296ace88a
Simeon J Morgan <smorgan@digitalfeed.net>
2022-08-18 11:54:17 +1000

    Session 10 - add search

--- src/community_db/views.py
+++ src/community_db/views.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
@@ -6,8 +7,13 @@ from .models import Person
 # FUNCTION BASED VIEWS
 
 
+# Searching only the first_name field
 def list_persons_with_template(request):
+    search_text = request.GET.get("search")
+
     persons = Person.objects.all()
+    if search_text:
+        persons = persons.filter(first_name__icontains=search_text)
     context = {"object_list": persons}
     return render(request, "community_db/person_list_in_base.html", context)
 
