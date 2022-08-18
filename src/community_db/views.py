80a5ffbfc1be655dfa258d57e6bc94ed43feef46
Simeon J Morgan <smorgan@digitalfeed.net>
2022-08-18 12:05:28 +1000

    Session 10 - Add search to generic view

--- src/community_db/views.py
+++ src/community_db/views.py
@@ -31,6 +31,25 @@ class PersonListView(ListView):
     model = Person
     template_name = "community_db/person_list_in_base.html"
 
+    def get_queryset(self):
+        # Get the default queryset - i.e. set of rows - that we want
+        # to filter
+        queryset = super().get_queryset()
+
+        # Get the search field value - but from self.request in the
+        # class based view
+        search_text = self.request.GET.get("search")
+
+        # Filter if we need to
+        if search_text:
+            search_filters = models.Q(first_name__icontains=search_text) | models.Q(
+                last_name__icontains=search_text
+            )
+            queryset = queryset.filter(search_filters)
+
+        # Return the queryset now that we have filtered it (if we need to)
+        return queryset
+
 
 class PersonDetailView(DetailView):
     model = Person
