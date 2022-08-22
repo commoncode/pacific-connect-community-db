81a9b9dcc62366ea00f46cffe766415bd33b8337
Simeon J Morgan <smorgan@digitalfeed.net>
2022-08-22 13:09:41 +1000

    Show message if no results

--- src/community_db/views.py
+++ src/community_db/views.py
@@ -2,25 +2,21 @@ from django.db import models
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
-from .forms import QuickSearchForm
 from .models import Person
 
 # FUNCTION BASED VIEWS
 
-# Searching the first name and last name fields using a Django form
+# Searching the first name and last name fields with text in the search box
 def list_persons_with_template(request):
-    form = QuickSearchForm(request.GET)
+    search_text = request.GET.get("search")
 
     persons = Person.objects.all()
-    search_text = ""
-    if form.is_valid():
-        search_text = form.cleaned_data["search"]
-        if search_text:
-            search_filters = models.Q(first_name__icontains=search_text) | models.Q(
-                last_name__icontains=search_text
-            )
-            persons = persons.filter(search_filters)
-    context = {"object_list": persons, "search_text": search_text, "form": form}
+    if search_text:
+        search_filters = models.Q(first_name__icontains=search_text) | models.Q(
+            last_name__icontains=search_text
+        )
+        persons = persons.filter(search_filters)
+    context = {"object_list": persons, "search_text": search_text}
     return render(request, "community_db/person_list_in_base.html", context)
 
 
