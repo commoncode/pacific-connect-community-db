f0363b9c752ccaafccecfc092d0cdcfa812f4006
Simeon J Morgan <smorgan@digitalfeed.net>
2022-08-25 12:32:03 +1000

    Switch to using ModelForm

--- src/community_db/forms.py
+++ src/community_db/forms.py
@@ -7,8 +7,8 @@ class QuickSearchForm(forms.Form):
     search = forms.CharField(max_length=100, required=False)
 
 
-class PersonForm(forms.Form):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    country = forms.ChoiceField(required=False, choices=Person.Countries.choices)
-    mobile_number = forms.CharField(max_length=20, required=False)
+class PersonForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        # fields = ["first_name", "last_name", "country", "mobile_number"]
+        exclude = []
