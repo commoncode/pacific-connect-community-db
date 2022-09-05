a768e06835797faffb51a4a4bc3a44563c9aa9bb
Simeon J Morgan <smorgan@digitalfeed.net>
2022-09-05 13:11:12 +1000

    Choices with blank option

--- src/community_db/forms.py
+++ src/community_db/forms.py
@@ -14,7 +14,9 @@ class PersonForm(forms.ModelForm):
 
 
 class PersonSearchForm(forms.Form):
+    COUNTRIES = [("", "")] + list(Person.Countries.choices)
+
     first_name = forms.CharField(max_length=100, required=False)
     last_name = forms.CharField(max_length=100, required=False)
-    country = forms.ChoiceField(required=False, choices=Person.Countries.choices)
+    country = forms.ChoiceField(required=False, choices=COUNTRIES)
     mobile_number = forms.CharField(max_length=20, required=False)
